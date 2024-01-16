b8f7371a8bb3f490414a136329a08b7a5b25d329
--get <--get>
2024-01-16 15:05:29 -0800

    Added a sorter method and counter methods

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ def main():
     file = "books/frankenstein.txt"
     text = get_text(file)
     num_words = get_num_words(text)
-    print(f"{num_words} words were found in the document.")
-    num_chars = get_num_letters(text)
-    print(f"{num_chars}")
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_sort_alpha(chars_dict)
+
+    print(f"--- Begin report of {file} ---")
+    print(f"{num_words} words were found in the document.\n")
+    
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times.")
+    
+    print(f"--- End report ---")
+                    
 
 def get_text(file):
     with open(file) as f:
@@ -16,17 +26,25 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-# counts the number of times each character appears in the text
-def get_num_letters(text):
-    letters = {}
-    count = 0
+def sort_on(d):
+    return d["num"]
 
-    for i in range(len(text)):
-        if text[i] not in letters:
-            count = 1
+def chars_dict_sort_alpha(chars_dict):
+    chars_list = []
+    for ch in chars_dict:
+        chars_list.append({"char": ch, "num": chars_dict[ch]})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
+
+# counts the number of times each character appears in the text
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            count =+ 1
-        letters = dict({text[i]: count})
-    return text[i]
+            chars[lowered] = 1
+    return chars
 
 main()
